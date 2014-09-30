08b642cc5adfcb2188de53435b1fe523a5208be5
closure <retval@me.com>
2014-09-30 17:39:56 +0800

    Initial Commit

---
+++ Library/RSCFLibrary/RSNoteSR/RSNoteSR/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RSNoteSR
+//
+//  Created by closure on 9/30/14.
+//  Copyright (c) 2014 closure. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
